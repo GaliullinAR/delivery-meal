7f44043a95e16f2c0d66226c6748852bdad7d7ac
GaliullinAR <galiullin.ar651@gmail.com>
2023-01-04 17:30:14 +0300

    добавил навигационную панель, обработчик событий по клику изменение цвета активной кнопки

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 let getMeal = document.querySelector('.registration__link');
-
+let nav = document.querySelector('.menu__list');
 let items = document.querySelectorAll('.item');
 
-let array = Array.from(items);
+let itemsArr = Array.from(items);
 
 getMeal.addEventListener('click', function (e) {
   e.preventDefault();
-  items[0].style.display = 'none';
-  items[1].style.display = 'block';
+  itemsArr[0].style.display = 'none';
+  itemsArr[1].style.display = 'block';
+});
+
+
+let lists = nav.querySelectorAll('.menu__list-item');
+let listsArr = Array.from(lists);
+
+function getIndex(list) {
+  let result = null;
+  list.forEach((item, index) => {
+    if (item.classList.contains('active')) {
+      result = index;
+    }
+  })
+  return result;
+}
+
+listsArr.forEach(item => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    let lastIdx = getIndex(listsArr);
+    console.log(lastIdx);
+    listsArr[lastIdx].classList.remove('active');
+    e.currentTarget.classList.add('active');
+  })
 })
